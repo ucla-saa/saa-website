5f0ba86a158ac8f44bbb50e12268076b152638b0
Neil Kardan <neilkardan@ucla.edu>
2023-08-08 14:23:19 -0700

    Organization, landingPage initialization

---
+++ src/screens/LandingPage.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from '../logo.svg';
+import '../styles/LandingPage.css';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button'
+
+function LandingPage() {
+  return (
+    <div style={{display: "flex", alignItems: "center", justifyContent: "center", height: "100%"}}>
+      <div className="Landing">
+        <h2> UCLA Student Alumni Association </h2>
+        <Stack className="Login">
+            <TextField fullWidth id="standard-basic" label="Email" variant="standard" />
+            <TextField fullWidth id="standard-basic" label="Password" variant="standard" />
+        </Stack>
+        <Button variant="contained">Login</Button>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
